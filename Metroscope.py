6fd6e8c3080df0c0f08f6a883b6905d55a6d5cc6
Mathieu Glachant <syvwlch@gmail.com>
2019-05-27 14:20:21 -0400

    Factored out the logic to mark stresses above the voiced vowels

--- Metroscope.py
+++ Metroscope.py
@@ -3,38 +3,35 @@
 import pronouncing as prn
 
 
-def stressMarker(text, stress_pattern):
-    """Mark stresses over vowels."""
+def stress_line(line, stress_pattern):
+    """Mark stresses over vowels in a line of text."""
     custom_dict = {"Phidian": "20"}
-    for verse in text.splitlines():
-        say = ""
-        stresses = stress_pattern
-        clean_verse = verse.replace("-", " ")
-        for word in clean_verse.split():
-            clean_word = word.replace("è", "e")
-            for punct in ".,;!?":
-                clean_word = clean_word.replace(punct, "")
+    say = ""
+    stresses = stress_pattern
+    clean_line = line.replace("-", " ")
+    for word in clean_line.split():
+        clean_word = word.replace("è", "e")
+        for punct in ".,;!?":
+            clean_word = clean_word.replace(punct, "")
+        try:
+            word_stresses = prn.stresses_for_word(clean_word)[0]
+        except IndexError:
             try:
-                word_stresses = prn.stresses_for_word(clean_word)[0]
+                word_stresses = custom_dict[clean_word]
             except IndexError:
-                try:
-                    word_stresses = custom_dict[clean_word]
-                except IndexError:
-                    word_stresses = ""
-            for char in word:
-                if char in "aeiouyAEIOUY" and len(word_stresses):
-                    say += stresses[0]
-                    stresses = stresses[1:]
-                    word_stresses = word_stresses[1:]
-                elif char in "è":
-                    say += stresses[0]
-                    stresses = stresses[1:]
-                else:
-                    say += " "
-            say += " "
-        print(say)
-        print(verse)
-    return
+                word_stresses = ""
+        for char in word:
+            if char in "aeiouyAEIOUY" and len(word_stresses):
+                say += stresses[0]
+                stresses = stresses[1:]
+                word_stresses = word_stresses[1:]
+            elif char in "è":
+                say += stresses[0]
+                stresses = stresses[1:]
+            else:
+                say += " "
+        say += " "
+    return say
 
 
 TESTTEXT = """
@@ -51,4 +48,6 @@ With vases, to one deep in Phidian lore."""
 
 TESTMETER = 'x/x/x/x/x/'
 
-stressMarker(TESTTEXT, TESTMETER)
+for line in TESTTEXT.splitlines():
+    print(stress_line(line, TESTMETER))
+    print(line)
