2b0c9c5ed1e39ac6e1b781b284f3bf3004c559f6
Mathieu Glachant <syvwlch@gmail.com>
2019-05-31 07:34:51 -0400

    Added Flask to provide output as html. Closes #1

--- OdeOnIndolence.py
+++ OdeOnIndolence.py
@@ -1,14 +1,32 @@
 """Analysis of Keat's Ode on Indolence."""
 
+from flask import Flask
 from Metroscope import stress_line
 
+app = Flask(__name__)
 POEM_PATH = "Texts/FreeTexts/OdeOnIndolence.txt"
 POEM_METER = "x/x/x/x/x/"
 
-if __name__ == "__main__":
-    with open(POEM_PATH, "r") as poem:
+
+@app.route("/")
+def home():
+    """Define the home route."""
+    return scanned_poem(POEM_PATH, POEM_METER)
+
+
+def scanned_poem(path, meter):
+    """Create a multiline string with the scanned poem."""
+    result = ""
+    with open(path, "r") as poem:
         for line in poem:
-            aligned_stresses, stressed_line = stress_line(line, POEM_METER)
-            print(aligned_stresses)
-            print(stressed_line)
+            aligned_stresses, stressed_line = stress_line(line, meter)
+            # result += aligned_stresses
+            # result += "<br>\n"
+            result += stressed_line
+            result += "<br>\n"
             # print(line, end="")
+    return result
+
+
+if __name__ == "__main__":
+    print(scanned_poem(POEM_PATH, POEM_METER))
