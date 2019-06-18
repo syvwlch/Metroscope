fde09483ca0aa75f67ffde87c266b99c83fa721d
Mathieu Glachant <syvwlch@gmail.com>
2019-06-18 11:14:59 -0400

    Cleaned up line and poem level to use WordBuilder class for word level

--- Metroscope.py
+++ Metroscope.py
@@ -119,91 +119,6 @@ run on the code above.
 """
 
 
-def clean_word(word):
-    """Prepare a word for CMU lookup."""
-    clean = word
-    # First, force lowercase and strip punctuation
-    clean = clean.lower()
-    for punct in ".,;:!?—'\"":
-        clean = clean.replace(punct, "")
-    # Many poets mark added stress on a silent e with an è
-    clean = clean.replace("è", "e")
-    # Many poets mark elided vowels with a ’ at the end of a wor
-    if clean[-2:] == "’s":
-        clean = clean.replace("’s", "")
-    clean = clean.replace("’d", "ed")
-    return clean
-
-
-def get_stress_word(word):
-    """Return a list of the stresses for the given word."""
-    custom_dict = {
-                    "phidian": "20",
-                    "indolence": "200",
-                    "benumbed": "02",
-                    "unhaunted": "020",
-                    "nothingness": "202",
-                    "embroidered": "020",
-                    "besprinkled": "020",
-                    "o’er": "0",
-                    "unmeek": "02",
-                    "poesy": "202",
-                    "forsooth": "02",
-                    "honeyed": "20",
-                    "casement": "20",
-                    "leaved": "2",
-                    "throstle": "20",
-                    "’twas": "2",
-                    "dieted": "202",
-                    "masque": "2",
-                    "spright": "2",
-                    "flow’rs": "2",
-                    "deniest": "20",
-                    "know’st": "2",
-                    "triumph’st": "20",
-                    "say’st": "2",
-                    "find’st": "2",
-                    "yield’st": "2",
-                    "’tis": "2",
-                    "purpled": "20",
-                    "maidenhead": "202",
-                    "orisons": "200",
-                    "mockeries": "200",
-                    "pallor": "20",
-                  }
-    cleaned_word = clean_word(word)
-    try:
-        word_stresses = custom_dict[cleaned_word]
-    except KeyError:
-        try:
-            word_stresses = stresses_for_word(cleaned_word)[0]
-        except IndexError:
-            word_stresses = ""
-    if "è" in word:
-        word_stresses += "1"
-    return list(word_stresses)
-
-
-def get_syllables_word(word):
-    """
-    Retrieve the syllables for a given word.
-
-    Returns a list of [syllable text, syllable stress].
-    """
-    syllables = SonoriPy(word.lower())
-    stresses = get_stress_word(word)
-    result = []
-    for syllable in syllables:
-        if len(stresses) > 1:
-            result.append([word[0:len(syllable)], stresses.pop(0)])
-            word = word[len(syllable):]
-        elif len(stresses) == 1:
-            result.append([word, stresses.pop(0)])
-        else:
-            pass
-    return result
-
-
 def clean_line(line):
     """Prepare a line of text for show_stress_line."""
     clean = line
@@ -230,31 +145,37 @@ def stress_word(word, stresses):
     """
     MATCH = "color:black"
     NOT_MATCH = "color:red"
-    syllables = get_syllables_word(word)
+    STRESSED = "strong"
+    UNSTRESSED = "span"
+    UNKNOWN = "small"
+
+    syllables = WordBuilder(word, custom_dict=CUSTOM_DICT).stressed_syllables
     result = ""
     for syllable, pronunciation_stress in syllables:
         if stresses:
             if stresses.pop(0):
                 if pronunciation_stress == '0':
-                    result += tag_string(syllable, "strong", NOT_MATCH)
+                    result += tag_string(syllable, STRESSED, NOT_MATCH)
                 else:
-                    result += tag_string(syllable, "strong", MATCH)
+                    result += tag_string(syllable, STRESSED, MATCH)
             else:
                 if pronunciation_stress == '2':
-                    result += tag_string(syllable, "span", NOT_MATCH)
+                    result += tag_string(syllable, UNSTRESSED, NOT_MATCH)
                 else:
-                    result += tag_string(syllable, "span", MATCH)
+                    result += tag_string(syllable, UNSTRESSED, MATCH)
         else:
-            result += tag_string(syllable, "small", NOT_MATCH)
+            result += tag_string(syllable, UNKNOWN, NOT_MATCH)
     return tag_string(result, "span")
 
 
 def stress_line(line, stress_pattern):
     """Mark up a line of verse based on the stress pattern provided."""
-    stressed_line = ""
+    MISSING = "<b style='color:red'> _ </b>"
+
     line = clean_line(line)
+    stressed_line = ""
     for word in line.split():
-        word_stresses = get_stress_word(word)
+        word_stresses = WordBuilder(word, custom_dict=CUSTOM_DICT).stresses
         number_stresses = len(word_stresses)
         word_meter = stress_pattern[0:number_stresses]
         stress_pattern = stress_pattern[number_stresses:]
@@ -263,7 +184,7 @@ def stress_line(line, stress_pattern):
         # -----------------------------------------------------
         stressed_line += stress_word(word, word_stresses) + " "
     for stress in stress_pattern:
-        stressed_line += "<b style='color:red'> _ </b>"
+        stressed_line += MISSING
     return stressed_line
 
 
