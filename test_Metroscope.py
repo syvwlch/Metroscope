04ca015284bb18afaaa49548f5743fab56bfae2b
Mathieu Glachant <syvwlch@gmail.com>
2019-06-19 09:16:49 -0400

    Switched to dicts for input/output test data.

--- test_Metroscope.py
+++ test_Metroscope.py
@@ -52,26 +52,22 @@ class Test_WordBuilder(unittest.TestCase):
 
     def test_syllables(self):
         """Should set the syllables from the original word."""
-        WORDS = (
-                 ("automatic",
-                  ['au', 'to', 'ma', 'tic']),
-                 ("serene",
-                  ['se', 're', 'ne']),
-                 )
-        for word, syllables in WORDS:
+        WORDS = {
+                 "automatic": ['au', 'to', 'ma', 'tic'],
+                 "serene": ['se', 're', 'ne'],
+                 }
+        for word, syllables in WORDS.items():
             with self.subTest('Original word: ' + word):
                 self.assertEqual(WordBuilder(word).syllables,
                                  syllables)
 
     def test_stresses(self):
         """Should set the stresses from the original word."""
-        WORDS = (
-                 ("automatic",
-                  ['2', '0', '1', '0']),
-                 ("serene",
-                  ['0', '1']),
-                 )
-        for word, stresses in WORDS:
+        WORDS = {
+                 "automatic": ['2', '0', '1', '0'],
+                 "serene": ['0', '1'],
+                 }
+        for word, stresses in WORDS.items():
             with self.subTest('Original word: ' + word):
                 self.assertEqual(WordBuilder(word).stresses,
                                  stresses)
@@ -91,13 +87,12 @@ class Test_WordBuilder(unittest.TestCase):
 
     def test_stressed_syllables(self):
         """Should be a list of the original word's syllables with stress."""
-        WORDS = (
-                 ("automatic",
-                  [['au', '2'], ['to', '0'], ['ma', '1'], ['tic', '0']]),
-                 ("serene",
-                  [['se', '0'], ['rene', '1']]),
-                 )
-        for word, stressed_syllables in WORDS:
+        WORDS = {
+                 "automatic":
+                 [['au', '2'], ['to', '0'], ['ma', '1'], ['tic', '0']],
+                 "serene": [['se', '0'], ['rene', '1']],
+                 }
+        for word, stressed_syllables in WORDS.items():
             with self.subTest('Original word: ' + word):
                 self.assertEqual(WordBuilder(word).stressed_syllables,
                                  stressed_syllables)
@@ -114,54 +109,59 @@ class Test_WordBuilder(unittest.TestCase):
 
     def test_word_has_grave_over_e(self):
         """Should change an 'è' to an 'e'."""
-        WORDS = (("belovèd", "beloved"),
-                 ("bowèd", "bowed"),
-                 ("joinèd", "joined"),
-                 )
-        for word, cleaned_word in WORDS:
+        WORDS = {
+                 "belovèd": "beloved",
+                 "bowèd": "bowed",
+                 "joinèd": "joined",
+                 }
+        for word, cleaned_word in WORDS.items():
             with self.subTest('Tried to clean: ' + word):
                 self.assertEqual(WordBuilder(word).clean_word,
                                  cleaned_word)
 
     def test_word_has_elision(self):
         """Should replace ’d with ed."""
-        WORDS = (("stepp’d", "stepped"),
-                 ("pass’d", "passed"),
-                 )
-        for word, cleaned_word in WORDS:
+        WORDS = {
+                 "stepp’d": "stepped",
+                 "pass’d": "passed",
+                 }
+        for word, cleaned_word in WORDS.items():
             with self.subTest('Tried to clean: ' + word):
                 self.assertEqual(WordBuilder(word).clean_word,
                                  cleaned_word)
 
     def test_word_has_possessive(self):
         """Should strip final ’s without touching longer strings."""
-        WORDS = (("pleasure’s", "pleasure"),
-                 ("man’s", "man"),
-                 ("know’st", "know’st"),
-                 )
-        for word, cleaned_word in WORDS:
+        WORDS = {
+                 "pleasure’s": "pleasure",
+                 "man’s": "man",
+                 "know’st": "know’st",
+                 }
+        for word, cleaned_word in WORDS.items():
             with self.subTest('Tried to clean: ' + word):
                 self.assertEqual(WordBuilder(word).clean_word,
                                  cleaned_word)
 
     def test_word_has_uppercase(self):
         """Should force lowercase."""
-        WORDS = (("Phidian", "phidian"),
-                 ("Shadows", "shadows"),
-                 ("One", "one"),
-                 )
-        for word, cleaned_word in WORDS:
+        WORDS = {
+                 "Phidian": "phidian",
+                 "Shadows": "shadows",
+                 "One": "one",
+                 }
+        for word, cleaned_word in WORDS.items():
             with self.subTest('Tried to clean: ' + word):
                 self.assertEqual(WordBuilder(word).clean_word,
                                  cleaned_word)
 
     def test_word_has_punctuation(self):
         """Should strip punctuation."""
-        WORDS = (("seen,", "seen"),
-                 ("faced;", "faced"),
-                 (".,;:!?—'\"", ""),
-                 )
-        for word, cleaned_word in WORDS:
+        WORDS = {
+                 "seen,": "seen",
+                 "faced;": "faced",
+                 ".,;:!?—'\"": "",
+                 }
+        for word, cleaned_word in WORDS.items():
             with self.subTest('Tried to clean: ' + word):
                 self.assertEqual(WordBuilder(word).clean_word,
                                  cleaned_word)
