db42afe71ca50d3b6f567d77ade965c62cd77394
Mathieu Glachant <syvwlch@gmail.com>
2019-07-05 11:04:31 -0400

    Refactored the sample data injection to static methods of the models This closes #96.

--- run/models.py
+++ run/models.py
@@ -17,6 +17,24 @@ class Meter(db.Model):
         """Represent the class."""
         return f"<Meter '{self.name}'>"
 
+    @staticmethod
+    def insert_samples():
+        """
+        Insert some sample meters into the database.
+
+        Idempotent.
+        """
+        METERS = [
+            {'name': 'Iambic Pentameter', 'pattern': '0101010101'},
+            {'name': 'Cataleptic Anapestic Trimeter', 'pattern': '01001001'}
+        ]
+        for meter in METERS:
+            if Meter.query.filter_by(pattern=meter['pattern']).first() is None:
+                db.session.add(
+                    Meter(name=meter['name'], pattern=meter['pattern'])
+                )
+        db.session.commit()
+
 
 class Poet(db.Model):
     """Define the Poets table."""
@@ -29,6 +47,24 @@ class Poet(db.Model):
     def __repr__(self):
         """Represent the class."""
         return f"<Poet '{self.name}'>"
+
+    @staticmethod
+    def insert_samples():
+        """
+        Insert some sample poets into the database.
+
+        Idempotent.
+        """
+        POETS = [
+            'John Keats',
+            'Edward Lear',
+            'John Donne',
+            'Wilfred Owen'
+        ]
+        for poet in POETS:
+            if Poet.query.filter_by(name=poet).first() is None:
+                db.session.add(Poet(name=poet))
+        db.session.commit()
 
 
 class Poem(db.Model):
@@ -47,77 +83,67 @@ class Poem(db.Model):
         """Represent the class."""
         return f"<Poem '{self.title}'>"
 
+    @staticmethod
+    def insert_samples():
+        """
+        Insert some sample poems into the database.
+
+        Idempotent.
+        """
+        POEMS = [
+            {
+                'title': 'Ode on Indolence',
+                'keyword': 'OdeOnIndolence',
+                'poet': 'John Keats',
+                'pattern': '0101010101',
+            },
+            {
+                'title': 'There Was an Old Man with a Beard',
+                'keyword': 'OldManWithBeard',
+                'poet': 'Edward Lear',
+                'pattern': '01001001',
+            },
+            {
+                'title': 'The Flea',
+                'keyword': 'Flea',
+                'poet': 'John Donne',
+                'pattern': '0101010101',
+            },
+            {
+                'title': 'Anthem for Doomed Youth',
+                'keyword': 'AnthemForDoomedYouth',
+                'poet': 'Wilfred Owen',
+                'pattern': '0101010101',
+            },
+        ]
+        for poem in POEMS:
+            if Poet.query.filter_by(name=poem['poet']).first() is None:
+                raise ValueError('This poet does not exist.')
+            if Meter.query.filter_by(pattern=poem['pattern']).first() is None:
+                raise ValueError('This meter pattern does not exist.')
+            if Poem.query.filter_by(keyword=poem['keyword']).first() is None:
+                path = 'samples/free/' + poem['keyword'] + '.txt'
+                title = poem['title']
+                keyword = poem['keyword']
+                poet = Poet.query.filter_by(name=poem['poet']).first()
+                meter = Meter.query.filter_by(pattern=poem['pattern']).first()
+                with open(path, "r") as text_file:
+                    raw_text = str(text_file.read())
+                    db.session.add(
+                        Poem(
+                            title=title,
+                            keyword=keyword,
+                            raw_text=raw_text,
+                            HTML=scanned_poem(raw_text, meter.pattern),
+                            poet_id=poet.id,
+                            meter_id=meter.id,
+                        )
+                    )
+        db.session.commit()
+
 
 def reset_db():
-    """Reset the database with the sample poems."""
-    METERS = [
-        {'name': 'Iambic Pentameter', 'pattern': '0101010101'},
-        {'name': 'Cataleptic Anapestic Trimeter', 'pattern': '01001001'}
-    ]
-    POETS = [
-        'John Keats',
-        'Edward Lear',
-        'John Donne',
-        'Wilfred Owen'
-    ]
-    POEMS = [
-        {
-            'title': 'Ode on Indolence',
-            'keyword': 'OdeOnIndolence',
-            'poet': 'John Keats',
-            'pattern': '0101010101',
-        },
-        {
-            'title': 'There Was an Old Man with a Beard',
-            'keyword': 'OldManWithBeard',
-            'poet': 'Edward Lear',
-            'pattern': '01001001',
-        },
-        {
-            'title': 'The Flea',
-            'keyword': 'Flea',
-            'poet': 'John Donne',
-            'pattern': '0101010101',
-        },
-        {
-            'title': 'Anthem for Doomed Youth',
-            'keyword': 'AnthemForDoomedYouth',
-            'poet': 'Wilfred Owen',
-            'pattern': '0101010101',
-        },
-    ]
-
-    for meter in METERS:
-        if Meter.query.filter_by(pattern=meter['pattern']).first() is None:
-            db.session.add(
-                Meter(name=meter['name'], pattern=meter['pattern'])
-            )
-
-    for poet in POETS:
-        if Poet.query.filter_by(name=poet).first() is None:
-            db.session.add(Poet(name=poet))
-
-    # commit so loading poets can use poets and meters tables
-    db.session.commit()
-
-    for poem in POEMS:
-        if Poem.query.filter_by(keyword=poem['keyword']).first() is None:
-            path = 'samples/free/' + poem['keyword'] + '.txt'
-            title = poem['title']
-            keyword = poem['keyword']
-            poet = Poet.query.filter_by(name=poem['poet']).first()
-            meter = Meter.query.filter_by(pattern=poem['pattern']).first()
-            with open(path, "r") as text_file:
-                raw_text = str(text_file.read())
-                db.session.add(
-                    Poem(
-                        title=title,
-                        keyword=keyword,
-                        raw_text=raw_text,
-                        HTML=scanned_poem(raw_text, meter.pattern),
-                        poet_id=poet.id,
-                        meter_id=meter.id,
-                    )
-                )
-
-    db.session.commit()
+    """Insert sample poems into the database."""
+    Meter.insert_samples()
+    Poet.insert_samples()
+    Poem.insert_samples()
