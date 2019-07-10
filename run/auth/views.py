3bd433c601e461b566d3519808a1b5e1e5af9806
Mathieu Glachant <syvwlch@gmail.com>
2019-07-10 14:33:26 -0400

    Added change password feature

--- run/auth/views.py
+++ run/auth/views.py
@@ -1,11 +1,11 @@
 """Route definitions for the auth blueprint."""
 
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from run import db
 from . import auth
 from ..models import User
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm, ChangePasswordForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -44,3 +44,19 @@ def register():
         flash('You can now login.')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
+
+
+@auth.route('/change-password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.old_password.data):
+            current_user.password = form.password.data
+            db.session.add(current_user)
+            db.session.commit()
+            flash('Your password has been updated.')
+            return redirect(url_for('main.home'))
+        else:
+            flash('Invalid password.')
+    return render_template("auth/change_password.html", form=form)
