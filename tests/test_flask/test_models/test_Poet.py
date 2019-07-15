a59d26fd0c6de4cb35629d5679c7592528777bbd
Mathieu Glachant <syvwlch@gmail.com>
2019-07-15 10:25:56 -0400

    Added a test for insert_sample() static method for Poet.

--- tests/test_flask/test_models/test_Poet.py
+++ tests/test_flask/test_models/test_Poet.py
@@ -8,3 +8,12 @@ def test_Poet_repr():
     assert repr(
         Poet(name='name')
     ) == "<Poet 'name'>"
+
+
+def test_Poet_insert_samples(app):
+    """Test the insert_samples static method of Poet."""
+    assert Poet.query.first() is None
+    Poet.insert_samples()
+    assert Poet.query.first() is not None
+    for poet in Poet.query.all():
+        assert isinstance(poet.name, str)
