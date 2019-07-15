ee1c451d0d67790741119dfce2124b579797a6e5
Mathieu Glachant <syvwlch@gmail.com>
2019-07-15 10:38:10 -0400

    Added a test for insert_samples() static method for Poem

--- tests/test_flask/test_models/test_Poem.py
+++ tests/test_flask/test_models/test_Poem.py
@@ -1,7 +1,7 @@
 """Test the Poem model."""
 
 import pytest
-from run.models import Poet, Poem
+from run.models import Meter, Poet, Poem
 
 
 def test_Poem_repr():
@@ -26,3 +26,18 @@ def test_Poem_ValueError(app):
     with pytest.raises(ValueError) as excinfo:
         Poem.insert_samples()
     assert "This meter pattern does not exist." in str(excinfo.value)
+
+
+def test_Poem_insert_samples(app):
+    """Test the insert_samples static method of Poem."""
+    assert Poem.query.first() is None
+    Meter.insert_samples()
+    Poet.insert_samples()
+    Poem.insert_samples()
+    assert Poem.query.first() is not None
+    for poem in Poem.query.all():
+        assert isinstance(poem.title, str)
+        assert isinstance(poem.keyword, str)
+        assert isinstance(poem.raw_text, str)
+        assert isinstance(poem.author, Poet)
+        assert isinstance(poem.meter, Meter)
