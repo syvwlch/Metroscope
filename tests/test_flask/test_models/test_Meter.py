b75cb8db6bcdc4df33d131744ce542dcd39331a5
Mathieu Glachant <syvwlch@gmail.com>
2019-07-15 10:16:19 -0400

    Added a test for the insert_samples() static method for Meter

--- tests/test_flask/test_models/test_Meter.py
+++ tests/test_flask/test_models/test_Meter.py
@@ -7,3 +7,17 @@ def test_Meter_repr():
     """Test the __repr__ method."""
     assert repr(
         Meter(name='name', pattern='pattern')) == "<Meter 'name'>"
+
+
+def test_Meter_insert_samples(app):
+    """Test the insert_samples static method of Meter."""
+    assert Meter.query.first() is None
+    Meter.insert_samples()
+    assert Meter.query.first() is not None
+    for meter in Meter.query.all():
+        assert 'meter' in meter.name
+
+        pattern = meter.pattern
+        for char in ['0', '1']:
+            pattern = pattern.replace(char, '')
+        assert pattern == ''
