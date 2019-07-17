2ac288f929407190af167ed0949f32e4c885d194
Mathieu Glachant <syvwlch@gmail.com>
2019-07-17 17:12:10 -0400

    Refactored logout tests

--- tests/test_flask/test_auth_blueprint/test_logout_view.py
+++ tests/test_flask/test_auth_blueprint/test_logout_view.py
@@ -1,6 +1,9 @@
 """Test the logout view using flask's test_client()."""
 
 
+from flask import url_for, get_flashed_messages
+
+
 def test_302(client):
     """Check the logout view should always return a 302."""
     assert "302" in client.get("/auth/logout").status
@@ -14,10 +17,11 @@ def test_logout_user(client, auth):
         auth.register()
         assert not current_user.is_authenticated
 
-        auth.login()
+        auth.login(follow_redirects=True)
         assert current_user.is_authenticated
 
-        response = auth.logout(follow_redirects=True)
+        response = auth.logout()
         assert not current_user.is_authenticated
-        assert "200" in response.status
-        assert b"You have been logged out." in response.data
+        assert "302" in response.status
+        assert url_for('main.home', _external=True) == response.location
+        assert get_flashed_messages() == ['You have been logged out.']
