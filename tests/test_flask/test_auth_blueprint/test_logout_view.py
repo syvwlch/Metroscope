7acd3405571dd9f02907e4f22719d8128b9b9a0c
Mathieu Glachant <syvwlch@gmail.com>
2019-07-18 07:52:33 -0400

    Added test for logout when user is already logged out.

--- tests/test_flask/test_auth_blueprint/test_logout_view.py
+++ tests/test_flask/test_auth_blueprint/test_logout_view.py
@@ -25,3 +25,20 @@ def test_logout_user(client, auth):
         assert "302" in response.status
         assert url_for('main.home', _external=True) == response.location
         assert get_flashed_messages() == ['You have been logged out.']
+
+
+def test_logout_unauthenticated_user(client, auth):
+    """Check the logout user route is safe for unauthenticated user."""
+    from flask_login import current_user
+
+    with client:
+        auth.register(follow_redirects=True)
+        assert not current_user.is_authenticated
+
+        response = auth.logout()
+        assert not current_user.is_authenticated
+        assert "302" in response.status
+        redirect = url_for('auth.login', _external=True)
+        redirect += '?next=%2Fauth%2Flogout'
+        assert redirect == response.location
+        assert get_flashed_messages() == ['Please log in to access this page.']
