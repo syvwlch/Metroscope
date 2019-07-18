0d5a7b73c182ea60fed277ceba8ae8bee8fbea97
Mathieu Glachant <syvwlch@gmail.com>
2019-07-18 07:41:45 -0400

    Completed tests for change password

--- tests/test_flask/test_auth_blueprint/test_change_password_view.py
+++ tests/test_flask/test_auth_blueprint/test_change_password_view.py
@@ -21,12 +21,13 @@ def test_change_password(client, auth):
     with client:
         auth.register()
         auth.login(follow_redirects=True)
+        assert current_user.is_authenticated
+        assert current_user.verify_password('cat')
 
         response = client.post(
             '/auth/change-password',
             data={
                 'old_password': 'cat',
-                'email': 'john@metro.scope',
                 'password': 'dog',
                 'password2': 'dog',
             },
@@ -35,3 +36,44 @@ def test_change_password(client, auth):
         assert "302" in response.status
         assert url_for('main.home', _external=True) == response.location
         assert get_flashed_messages() == ['Your password has been updated.']
+        assert current_user.verify_password('dog')
+
+
+def test_change_password_no_old_password(client, auth):
+    """Check the change password route fails without old password."""
+    with client:
+        auth.register()
+        auth.login(follow_redirects=True)
+
+        response = client.post(
+            '/auth/change-password',
+            data={
+                'password': 'dog',
+                'password2': 'dog',
+            },
+        )
+        assert current_user.is_authenticated
+        assert "200" in response.status
+        assert get_flashed_messages() == []
+        assert b"This field is required." in response.data
+        assert current_user.verify_password('cat')
+
+
+def test_change_password_wrong_old_password(client, auth):
+    """Check the change password route fails with wrong old password."""
+    with client:
+        auth.register()
+        auth.login(follow_redirects=True)
+
+        response = client.post(
+            '/auth/change-password',
+            data={
+                'old_password': 'rabbit',
+                'password': 'dog',
+                'password2': 'dog',
+            },
+        )
+        assert current_user.is_authenticated
+        assert "200" in response.status
+        assert get_flashed_messages() == ['Invalid password.']
+        assert current_user.verify_password('cat')
