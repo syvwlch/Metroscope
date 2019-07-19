88909f82a815f680fb7a789490b0005e6f532b8a
Mathieu Glachant <syvwlch@gmail.com>
2019-07-19 15:48:21 -0400

    Added test for the poem blueprint

---
+++ tests/test_flask/test_poem_blueprint/test_poem_view.py
@@ -0,0 +1,9 @@
+"""Test the poem view using flask's test_client()."""
+
+
+def test_200(client_poems):
+    """Check the poem should always return a 200."""
+    response = client_poems.get('/poem/Flea')
+    assert b'Flea' in response.data
+    assert "200" in response.status
+    assert "404" in client_poems.get('/poem/foo').status
