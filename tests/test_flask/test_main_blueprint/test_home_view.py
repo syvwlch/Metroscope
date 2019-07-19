b85524b7661bd10c8bc5ce11c4cda581a1418f3b
Mathieu Glachant <syvwlch@gmail.com>
2019-07-19 17:03:56 -0400

    Removed the poem list from home page

--- tests/test_flask/test_main_blueprint/test_home_view.py
+++ tests/test_flask/test_main_blueprint/test_home_view.py
@@ -1,6 +1,11 @@
 """Test the home view using flask's test_client()."""
 
+from flask import url_for
+
 
 def test_200(client):
     """Check the home should always return a 200."""
-    assert "200" in client.get("/").status
+    with client:
+        response = client.get("/")
+        assert "200" in response.status
+        assert url_for('poetry.poem_list') in str(response.data)
--- tests/test_flask/test_poetry_blueprint/test_add_samples_view.py
+++ tests/test_flask/test_poetry_blueprint/test_add_samples_view.py
@@ -5,7 +5,7 @@ from run import db
 def test_dropped_db(client):
     """Check behavior when the db has no tables."""
     db.drop_all()
-    assert b'Sorry' in client.get('/').data
+    assert b'Sorry' in client.get('/poetry/poem').data
     assert "404" in client.get('/poetry/poem/Flea').status
     # /add_samples route should not cause server error when db has no tables
     assert "404" in client.get('/poetry/add_samples').status
@@ -16,12 +16,12 @@ def test_add_samples_route(client):
     # The database has tables in it
     assert "poems" in db.engine.table_names()
     # The database has no poems in it
-    assert b'Sorry' in client.get('/').data
+    assert b'Sorry' in client.get('/poetry/poem').data
     assert "404" in client.get('/poetry/poem/Flea').status
 
     # /add_samples route injects sample poems into database
     client.get('/poetry/add_samples')
-    assert b'Flea' in client.get('/').data
+    assert b'Flea' in client.get('/poetry/poem').data
     response = client.get('/poetry/poem/Flea')
     assert b'Flea' in response.data
     assert "200" in response.status
@@ -29,7 +29,7 @@ def test_add_samples_route(client):
 
     # Makes sure doing it twice doesn't break anything.
     client.get('/poetry/add_samples')
-    assert b'Flea' in client.get('/').data
+    assert b'Flea' in client.get('/poetry/poem').data
     response = client.get('/poetry/poem/Flea')
     assert b'Flea' in response.data
     assert "200" in response.status
