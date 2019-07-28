bcb1e863fabede821d513d6afd05473669f32965
Mathieu Glachant <syvwlch@gmail.com>
2019-07-28 18:38:22 -0400

    Removed the permission check to change meter Website is much less useful if you need to log in to even see the effects of different meters!

--- run/poetry/views.py
+++ run/poetry/views.py
@@ -1,10 +1,9 @@
 """Route definitions for the poetry blueprint."""
 
 from flask import render_template, redirect, url_for
-from flask_login import current_user
 from run import db
 from . import poetry
-from ..models import Meter, Poet, Poem, Permission
+from ..models import Meter, Poet, Poem
 from .helpers import stanzas
 from .forms import ChangeMeterForm
 
@@ -28,28 +27,23 @@ def poem(keyword):
     # retrieve the requested poem if it exists
     poem = Poem.query.filter_by(keyword=keyword).first_or_404()
 
-    if current_user.can(Permission.CHANGE_METER):
-        form = ChangeMeterForm()
-        meters = Meter.query.order_by('name').all()
-        # move the poem's default meter to the top of the drop-down
-        meters.insert(0, meters.pop(meters.index(poem.meter)))
-        form.pattern.choices = [
-            (
-                m.pattern,
-                f'Scanned in {m.name}',
-            ) for m in meters
-        ]
+    form = ChangeMeterForm()
+    meters = Meter.query.order_by('name').all()
+    # move the poem's default meter to the top of the drop-down
+    meters.insert(0, meters.pop(meters.index(poem.meter)))
+    form.pattern.choices = [
+        (
+            m.pattern,
+            f'Scanned in {m.name}',
+        ) for m in meters
+    ]
 
-        if form.validate_on_submit():
-            pattern = form.pattern.data
-            meter = Meter.query.filter_by(pattern=pattern).first().name
-        else:
-            pattern = poem.meter.pattern
-            meter = poem.meter.name
+    if form.validate_on_submit():
+        pattern = form.pattern.data
+        meter = Meter.query.filter_by(pattern=pattern).first().name
     else:
-        form = None
-        meter = poem.meter.name
         pattern = poem.meter.pattern
+        meter = poem.meter.name
 
     return render_template(
         "poetry/poem.html",
