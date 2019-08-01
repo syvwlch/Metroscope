3edc8136e2b670718497a49e7f964caafa02d950
Mathieu Glachant <syvwlch@gmail.com>
2019-08-01 08:24:15 -0400

    Layout tweaks for readability

--- run/auth/forms.py
+++ run/auth/forms.py
@@ -11,7 +11,11 @@ class LoginForm(FlaskForm):
     """Define the login form."""
     email = StringField(
         'Email',
-        validators=[DataRequired(), Length(1, 64), Email()]
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Email(),
+        ]
     )
     password = PasswordField(
         'Password',
@@ -22,16 +26,37 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
-                                             Email()])
-    display_name = StringField('Display Name', validators=[
-        DataRequired(), Length(1, 64),
-        Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-               'Display names must have only letters, numbers, dots or '
-               'underscores')])
-    password = PasswordField('Password', validators=[
-        DataRequired(), EqualTo('password2', message='Passwords must match.')])
-    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Email(),
+        ],
+    )
+    display_name = StringField(
+        'Display Name',
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Regexp(
+                '^[A-Za-z][A-Za-z0-9_.]*$',
+                0,
+                'Display names must have only letters, numbers, dots or '
+                'underscores'),
+        ],
+    )
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password2', message='Passwords must match.'),
+        ],
+    )
+    password2 = PasswordField(
+        'Confirm password',
+        validators=[DataRequired()]
+    )
     recaptcha = RecaptchaField()
     submit = SubmitField('Register')
 
@@ -45,22 +70,46 @@ class RegistrationForm(FlaskForm):
 
 
 class ChangePasswordForm(FlaskForm):
-    old_password = PasswordField('Old password', validators=[DataRequired()])
-    password = PasswordField('New password', validators=[
-        DataRequired(), EqualTo('password2', message='Passwords must match.')])
-    password2 = PasswordField('Confirm new password',
-                              validators=[DataRequired()])
+    old_password = PasswordField(
+        'Old password',
+        validators=[DataRequired()]
+    )
+    password = PasswordField(
+        'New password',
+        validators=[
+            DataRequired(),
+            EqualTo('password2', message='Passwords must match.'),
+        ],
+    )
+    password2 = PasswordField(
+        'Confirm new password',
+        validators=[DataRequired()],
+    )
     submit = SubmitField('Update Password')
 
 
 class PasswordResetRequestForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
-                                             Email()])
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Email(),
+        ],
+    )
     submit = SubmitField('Reset Password')
 
 
 class PasswordResetForm(FlaskForm):
-    password = PasswordField('New Password', validators=[
-        DataRequired(), EqualTo('password2', message='Passwords must match')])
-    password2 = PasswordField('Confirm Password', validators=[DataRequired()])
+    password = PasswordField(
+        'New Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password2', message='Passwords must match'),
+        ],
+    )
+    password2 = PasswordField(
+        'Confirm Password',
+        validators=[DataRequired()],
+    )
     submit = SubmitField('Reset Password')
